7e83c6e0b8b275cce714e8bdc47e5a2f5196152e
kenuocn <1402992668@qq.com>
2017-12-21 00:15:49 +0800

    python定义input和字符串格式化使用

---
+++ demo1/input.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+#Python 输入框写法
+
+hieght = input('请输入你的身高:')
+
+print("你的身高是%s"%hieght)
+
+print('=====================\n')
+#Python 输入框写法
+
+age = 21 #定义年龄21岁
+
+print("我的年龄是:%d"%age)
