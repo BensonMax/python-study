a2b681aa8bd3161d0816860a12417982f72f5ecb
kenuocn <1402992668@qq.com>
2017-12-20 23:02:34 +0800

    python注释

---
+++ demo1/Python注释.py
@@ -0,0 +1,20 @@
+#这是单行注释
+#注释不会与程序有任何关系.只是对程序代码解释.提醒开发者某段代码的含义.
+print('hello word')
+
+'''
+这是Python多行注释,也不会被程序执行.
+print('hello word')
+print('hello word')
+print('hello word')
+'''
+print('hello word\n')
+
+print('第一天学习Python,好高兴呀!!!\n')
+
+"""
+laravel-china是个牛逼的社区,一直被模仿,从未被超越!!!
+这是多行双引号注释,不会被程序执行
+"""
+
+print('laravel-china是个牛逼的社区,一直被模仿,从未被超越!!!\n')
