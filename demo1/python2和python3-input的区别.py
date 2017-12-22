cd1f3524ed556c9161b89ff28f2e9d0642b010c7
kenuocn <1402992668@qq.com>
2017-12-22 21:24:13 +0800

    python2和python3的区别

---
+++ demo1/python2和python3-input的区别.py
@@ -0,0 +1,11 @@
+#-*- coding:utf-8 -*-
+
+name = input('你的名字是?')
+age = input('你的年龄是?')
+qq = input('你的qq是?')
+
+print("======================")
+print ('我的名字是%s'%name)
+print ('我的年龄是%s'%age)
+print ('我的QQ是%s'%qq)
+print("======================")
