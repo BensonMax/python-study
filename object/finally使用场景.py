4b396ad2dd495aed35f4df7303efcded30489caf
zhanggaoyuan <zhanggaoyuan@singulato.com>
2018-04-03 01:22:44 +0800

    python 多个异常处理等.

---
+++ object/finally使用场景.py
@@ -0,0 +1,20 @@
+import time
+try:
+    f = open('test.txt')
+    try:
+        while True:
+            content = f.readline()
+            if len(content) == 0:
+                break
+            time.sleep(2)
+            print(content)
+    except Exception:
+        print('出现异常')
+        #如果在读取文件的过程中，产生了异常，那么就会捕获到
+        #比如 按下了 ctrl+c
+        pass
+    finally:
+        f.close()
+        print('关闭文件')
+except:
+    print("没有这个文件")
